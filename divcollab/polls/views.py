2bec8105539906c31027878a34c48710453765c7
Christina Nguyen <phuonghien716@gmail.com>
2015-11-28 16:27:58 -0500

    Added URL drections.

--- divcollab/polls/views.py
+++ divcollab/polls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+	return HttpResponse("Polls Index.")
+
+def detail(request, question_id):
+	return HttpResponse("Question: %s." % question_id)
+
+def results(request, question_id):
+	response = "Results for question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("Enter vote for question %s." % question_id)
